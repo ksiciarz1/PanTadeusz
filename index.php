e3b63822fbcddbbd0f8b2fc10a09c21d5e427f9b
ksiciarz1 <kacpersiciarz2001@gmail.com>
2022-11-08 12:44:06 +0100

    changed to php added image, header, footer, bootstrap

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css%22 rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js%22 integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js%22 integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+    <title>Pan Tadeusz</title>
+</head>
+<body>
+    <header class="containter text-center">
+        <h1>Pan Tadeusz, czyli ostatni zajazd na Litwie: historia szlachecka z roku 1811 i 1812 we dwunastu księgach wierszem</h1>
+
+    </header>
+    <div>
+        <h2>Spis tresci</h2>
+        <ul> <!-- OSTYLOWAĆ PRZY UŻYCIU BOOTSTRAP -->
+            <li><a href="./k1.html">Księga 1</a></li>
+            <li><a href="./k2.html">Księga 2</a></li>
+            <li><a href="./k3.html">Księga 3</a></li>
+            <li><a href="./k4.html">Księga 4</a></li>
+            <li><a href="./k5.html">Księga 5</a></li>
+            <li><a href="./k6.html">Księga 6</a></li>
+            <li><a href="./k7.html">Księga 7</a></li>
+            <li><a href="./k8.html">Księga 8</a></li>
+            <li><a href="./k9.html">Księga 9</a></li>
+            <li><a href="./k10.html">Księga 10</a></li>
+            <li><a href="./k11.html">Księga 11</a></li>
+            <li><a href="./k12.html">Księga 12</a></li>
+        </ul>
+        <img src="./Pan-Tadeusz--w-rezyserii-Andrzeja-Wajd.jpg" alt="Pan Tadeusz">
+    </div>
+    <div>
+        <?php
+            include_once("./k1.html");
+        ?> 
+    </div>
+    <footer class="containter">
+        <p>&copy; Kacper Siciarz</p>
+    </footer>
+</body>
+</html>
